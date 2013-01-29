c9c20db276b94f7336ccb650360cb0084944f02e
Matthew <Matthew@Matthew-Laptop>
2013-01-29 10:03:35 -0700

    added page ancestor function

--- wordpress/wp-content/themes/rare/functions.php
+++ wordpress/wp-content/themes/rare/functions.php
@@ -37,3 +37,15 @@ function page_categories_meta_box() {
     add_meta_box('categorydiv', __('Categories'), 'post_categories_meta_box', 'page', 'side', 'core');
 }
 add_action('admin_menu', 'page_categories_meta_box');
+
+
+// Check if page is an ancestor
+function is_ancestor($post_id) {
+    global $wp_query;
+    $ancestors = $wp_query->post->ancestors;
+    if ( in_array($post_id, $ancestors) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
