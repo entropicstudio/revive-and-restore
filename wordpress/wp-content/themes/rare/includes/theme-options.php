7259850fe2810c8905adf3a398e6ab3f33fc1e12
Matthew <Matthew@Matthew-Laptop>
2013-01-30 14:46:16 -0700

    Added theme options

--- wordpress/wp-content/themes/rare/includes/theme-options.php
+++ wordpress/wp-content/themes/rare/includes/theme-options.php
@@ -37,19 +37,21 @@ if ( function_exists('register_sidebar') ) {
 function rare_theme_menu() {
 
 	add_theme_page(
-		'Revive and Restore Theme', 			// The title to be displayed in the browser window for this page.
-		'RARE Theme Options',			// The text to be displayed for this menu item
-		'administrator',			// Which type of users can see this menu item
-		'rare_theme_options',	// The unique ID - that is, the slug - for this menu item
-		'rare_theme_display'		// The name of the function to call when rendering this menu's page
+		'Revive and Restore Theme', 	// Browser Page Title
+		'RARE Theme Options',			// Menu Label
+		'administrator',                // Which users to allow
+		'rare_theme_options',           // Menu id
+		'rare_theme_display'            // function callback
 	);
 
 }
 add_action('admin_menu', 'rare_theme_menu');
 
+
+// options page
 function rare_theme_display() { ?>
 
-<!-- default WordPress 'wrap' container -->  
+    <!-- default WordPress 'wrap' container -->  
     <div class="wrap">  
   
         <!-- Add the icon to the page -->  
@@ -71,9 +73,7 @@ function rare_theme_display() { ?>
 <?php	
 }
 
-/* ------------------------------------------------------------------------ *
- * Setting Registration
- * ------------------------------------------------------------------------ */ 
+// theme options settings
  
 function rare_initialize_theme_options() {
     
@@ -81,26 +81,25 @@ function rare_initialize_theme_options() {
         add_option( 'rare_general_options' );  
     } 
     
-	// First, we register a section. This is necessary since all future options must belong to a 
+	// create a options section 
 	add_settings_section(
-		'general_settings_section',			// ID used to identify this section and with which to register options
-		'General Options',					// Title to be displayed on the administration page
-		'rare_general_options_callback',	// Callback used to render the description of the section
-		'rare_general_options'                           // Page on which to add this section of options
+		'rare_general_settings_section',	// Section ID
+		'General Options',					// Section Title
+		'rare_general_options_callback',	// Section Description callback
+		'rare_general_options'              // Page to add section
 	);
 	
-	// Next, we'll introduce the fields for toggling the visibility of content elements.
+	// Fields in this section.
 	add_settings_field(	
-		'painting_text',						// ID used to identify the field throughout the theme
-		'Gone Painting Text',					// The label to the left of the option interface element
-		'rare_painting_text_callback',          // The name of the function responsible for rendering the option interface
-		'rare_general_options',                 // The page on which this option will be displayed
+		'painting_text',						// Field ID
+		'Gone Painting Text',					// Field Label
+		'rare_painting_text_callback',          // Field options callback
+		'rare_general_options',                 // Page to add field
 		'general_settings_section'
 	);
 
 	
-	// Finally, we register the fields with WordPress
-	
+	// Register all fields with WordPress
 	register_setting(
 		'rare_general_options',
 		'rare_general_options'
@@ -110,27 +109,22 @@ function rare_initialize_theme_options() {
 }
 add_action('admin_init', 'rare_initialize_theme_options');
 
-/* ------------------------------------------------------------------------ *
- * Section Callbacks
- * ------------------------------------------------------------------------ */ 
+
+// theme section callbacks
 
 function rare_general_options_callback() {
 	echo '<p>General Options for the Revive and Restore theme.</p>';
 } 
 
-/* ------------------------------------------------------------------------ *
- * Field Callbacks
- * ------------------------------------------------------------------------ */ 
+// field callbacks
 
 function rare_painting_text_callback($args) {
 	
+    // load options
     $options = get_option('rare_general_options');  
     
-	// Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
 	$html = '<textarea rows="4" cols="80" id="painting_text" name="rare_general_options[painting_text]">' . $options['painting_text'] . '</textarea>'; 
-	
-	// Here, we'll take the first argument of the array and add it to a label next to the checkbox
-	$html .= '<label for="painting_text"> '  . $args[0] . '</label>'; 
+	$html .= '<label for="painting_text"> '  . $args[0] . '</label>'; // first argument as label
 	
 	echo $html;
 	
