94531b4920283a6069b2e3bf1802409142962ab5
Matthew <Matthew@Matthew-Laptop>
2013-01-30 12:29:58 -0700

    added tedx template pages

---
+++ wordpress/wp-content/themes/rare/content-tedxdeextinction.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * content template for the TEDxDeExtinction homepage
+ */
+?>
+
+<div class="span-13 prepend-1 append-1">
+    
+    <?php the_content(); ?>t
+
+</div>
+
+
+    <?php get_template_part('sidebar', 'tedxdeextinction'); ?>
