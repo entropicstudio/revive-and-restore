388aecedf7c09c64b7fce89151ff2bb307d72a87
Matthew <Matthew@Matthew-Laptop>
2013-02-05 14:54:01 -0700

    updated staff & advisors template

--- wordpress/wp-content/themes/rare/content-people.php
+++ wordpress/wp-content/themes/rare/content-people.php
@@ -5,14 +5,16 @@
 ?>
 
 
-        <div class="span-9 prepend-1 append-2">
-                
-            <?php the_content(); ?>
+        <div class="span-12 prepend-1 append-1" id="staff_column">
+            <h2 class="section-heading">STAFF</h2>
+            <hr />
+            <?php the_field('column_1'); ?>
 
         </div>
 
-        <div class="span-9 last append-1">
-
+        <div class="span-7 last append-1" id="advisors_column">
+            <h2 class="section-heading">ADVISORS</h2>
+            <hr />
             <?php the_field('column_2'); ?>
 
         </div>
