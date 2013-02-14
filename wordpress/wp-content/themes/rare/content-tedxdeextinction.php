5249c2a807ce238b12f134b364bb67261a757223
Matthew <Matthew@Matthew-Laptop>
2013-02-14 14:49:22 -0700

    modified tedx homepage to show content instead of a post list

--- wordpress/wp-content/themes/rare/content-tedxdeextinction.php
+++ wordpress/wp-content/themes/rare/content-tedxdeextinction.php
@@ -5,24 +5,18 @@
 ?>
 
 <div class="span-13 prepend-1 append-1">
-    &nbsp;
+&nbsp;
     <?php // query_posts('category_id=7'); ?>
     
-    <?php
-        $args = array(
-          'post_type' => 'page',
-          'post__in' => array(355, 340)
-          );
-        query_posts($args);
-    ?>
-    
-    <?php while (have_posts()) : the_post(); ?>
+    <?php // while (have_posts()) : the_post(); ?>
         
-        <?php get_template_part('content', 'list'); ?>
+        <?php // get_template_part('content', 'list'); ?>
 
-    <?php endwhile; ?>
+    <?php // endwhile; ?>
     
-     <?php wp_reset_query(); ?> 
+     <?php // wp_reset_query(); ?>
+    
+    <?php the_content(); ?>
 
 </div>
 
