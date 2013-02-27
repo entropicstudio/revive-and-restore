443096605b28c8f048803a172f0e1511acfb9797
Matthew Brown <matthew@sansink.com>
2013-02-27 14:26:02 -0700

    Added species caption styles
    Species Captions have the class 'species-caption'.
    To Locate a caption over an image, use the classes 'top', 'middle',
    'bottom' and 'left', 'center', 'right'. e.g. class="species-caption
    middle right" will put the caption on the right side of the image and
    centered from top to bottom.

--- wordpress/wp-content/themes/rare/content-home.php
+++ wordpress/wp-content/themes/rare/content-home.php
@@ -6,7 +6,10 @@
 
     <div class="tedx_wrapper">
         <div class="top-shadow"></div>
-        <div id="cuban-macaw-homepage"><img src="<?php echo get_template_directory_uri(); ?>/ui/macaw-homepage-tedx.png" /></div>
+        <div id="cuban-macaw-homepage">
+            <div class="species-caption right middle"><strong>Cuban Red Macaw</strong><br /><em>Extinct: late 19th century</em></div>
+            <img src="<?php echo get_template_directory_uri(); ?>/ui/macaw-homepage-tedx.png" />
+        </div>
         <?php the_field('section-1'); ?>
     </div>
 
