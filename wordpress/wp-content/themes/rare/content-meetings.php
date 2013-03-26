ffb10848576c6b668e7237a602139a44bd36de07
Matthew Brown <matthew@sansink.com>
2013-03-26 10:45:25 -0600

    Updated Meetings Page, Fixes issue #134

--- wordpress/wp-content/themes/rare/content-meetings.php
+++ wordpress/wp-content/themes/rare/content-meetings.php
@@ -4,24 +4,28 @@
  */
 ?>
 
-
-<div class="tedx-box">
-    <?php the_field('section_1'); ?>
-    <br class="clear" />
-</div>
+<?php if (the_content()) { ?>
+    <div class="meetings-content-box">
+        <?php the_content(); ?>
+    </div>
+<?php } ?>
 
 <div id="meetings-boxes">
     
-    <?php the_field('box_1'); ?>
-    <br class="clear" />
-    <?php the_field('box_2'); ?>
+    <div class="box">
+        <?php the_field('box_1'); ?>
+    </div>
+   
+    <div class="box">
+        <?php the_field('box_2'); ?>
+    </div>
+    
+    <div class="box last">
+        <?php the_field('box_3'); ?>
+    </div>
+    
     <br class="clear" />
     
 </div>
 
-<div id="meetings-content-box">
-    
-    <?php the_content(); ?>
-    
-</div>
 <div class="species-caption right bottom white"><strong>Female/Male Labrador Duck</strong></div>
