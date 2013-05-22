6950d48feb41bb9140fd6573c40a2433ac86d40c
Matthew Brown <matthew@sansink.com>
2013-05-22 15:09:28 -0600

    Added new homepage grid layout

--- wordpress/wp-content/themes/rare/content-home.php
+++ wordpress/wp-content/themes/rare/content-home.php
@@ -1,45 +1,59 @@
 <?php
 /**
- * content template for the home page
+ * content template for the home pag
  */
 ?>
-
-    <div class="tedx_wrapper">
-        <div class="top-shadow"></div>
-        <div id="cuban-macaw-homepage">
-            <div class="species-caption right middle"><strong>Cuban Red Macaw</strong><br /><em>Extinct: late 19th century</em></div>
-            <img src="<?php echo get_template_directory_uri(); ?>/ui/macaw-homepage-tedx.png" />
+    
+    <?php if ( the_content() ) { ?>
+    <div class="span-20 prepend-1 append-1 padbottom-15">
+        <?php the_content(); ?>
+    </div>
+    <?php } ?>
+    
+    <div class="homepage-box-section first">
+        
+        <div class="box-grid first boxborder">
+            <?php the_field('box-1'); ?>
         </div>
-        <?php the_field('section-1'); ?>
-    </div>
-    
 
-    <div id="home-section-2">
-        <?php the_field('section-2'); ?>
-    </div>
-    
-    <br class="clear" />
+        <div class="box-grid boxborder">
+            <?php the_field('box-2'); ?>
+        </div>
 
-    <div id="home-section-3">
-        <?php the_field('section-3'); ?>
+        <div class="box-grid last">
+                <?php the_field('box-3'); ?>
+        </div>
+        
     </div>
 
-    <br class="clear" />
+    <div class="homepage-box-section">
+        
+        <div class="box-grid first boxborder">
+            <?php the_field('box-4'); ?>
+        </div>
 
-    <div id="home-section-4">
-        <?php the_field('section-4'); ?>
+        <div class="box-grid boxborder">
+            <?php the_field('box-5'); ?>
+        </div>
+
+        <div class="box-grid last">
+            <?php the_field('box-6'); ?>
+        </div>
+        
     </div>
-    
-    <div id="home-section-5">
-        <?php the_field('section-5'); ?>
-        <br class="clear" />
-    </div>
-    
-    <div id="home-section-6">
-        <?php the_field('section-6'); ?>
-    </div>
-    
-    <div id="home-section-7">
-        <?php the_field('section-7'); ?>
-        <br class="clear" />
+
+    <div class="homepage-box-section last">
+        
+        <div class="box-grid first boxborder">
+            <?php the_field('box-7'); ?>
+        </div>
+
+        <div class="box-grid boxborder">
+            <?php the_field('box-8'); ?>
+        </div>
+
+        <div class="box-grid last">
+            <?php the_field('box-9'); ?>
+        </div>
+        
     </div>
