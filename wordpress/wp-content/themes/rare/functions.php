77ab8b24f2df0186344c0125f068b6cd5d8035f6
Matthew Brown <matthew@sansink.com>
2013-05-22 15:20:32 -0600

    Removed Categories for pages

--- wordpress/wp-content/themes/rare/functions.php
+++ wordpress/wp-content/themes/rare/functions.php
@@ -11,13 +11,6 @@ require_once('includes/theme-login.php'); // custom login page
 // add wordpress editor stylesheet
 add_editor_style('css/editor.css');
 
-// add categories to pages
-function page_categories_meta_box() {
-    add_meta_box('categorydiv', __('Categories'), 'post_categories_meta_box', 'page', 'side', 'core');
-}
-add_action('admin_menu', 'page_categories_meta_box');
-
-
 // Check if page is an ancestor
 function is_ancestor($post_id) {
     global $wp_query;
