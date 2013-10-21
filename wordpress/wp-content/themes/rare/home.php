35de26d7bfbd469feacb8407017b16ccd7a9d080
Matthew <Matthew@Matthew-Laptop>
2013-10-21 07:39:34 -0600

    Display full first blog post

--- wordpress/wp-content/themes/rare/home.php
+++ wordpress/wp-content/themes/rare/home.php
@@ -11,47 +11,50 @@ get_header();
 
 
 
-            <div class="span-14 prepend-1 append-1">
+<div class="span-14 prepend-1 append-1">
 
-            <?php if (have_posts()) : ?>
-
-
-                    <?php /* Start the Loop */ ?>
-                    <?php while (have_posts()) : the_post(); ?>
-
-                            <?php get_template_part('content', 'list'); ?>
-                  
-                <?php endwhile; ?>
+    <?php if (have_posts()) : ?>
 
 
-            <?php else : ?>
+        <?php /* Start the Loop */ 
+        global $x;
+        $x = 1;
+        ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-                    <article id="post-0" class="post no-results not-found">
-                        <header class="entry-header">
-                            <h2 class="entry-title">Page Not Found</h2>
-                        </header><!-- .entry-header -->
-                        <hr />
-                        <div class="entry-content">
-                            <p>Apologies, but no results were found for the requested page.</p>
+            <?php get_template_part('content', 'list'); ?>
+
+        <?php endwhile; ?>
+
+
+    <?php else : ?>
+
+        <article id="post-0" class="post no-results not-found">
+            <header class="entry-header">
+                <h2 class="entry-title">Page Not Found</h2>
+            </header><!-- .entry-header -->
+            <hr />
+            <div class="entry-content">
+                <p>Apologies, but no results were found for the requested page.</p>
                 <?php get_search_form(); ?>
-                        </div><!-- .entry-content -->
-                    </article><!-- #post-0 -->
+            </div><!-- .entry-content -->
+        </article><!-- #post-0 -->
 
-            <?php endif; ?>
+    <?php endif; ?>
 
-            </div>
-
-
-            <!-- sidebar -->
-            <div class="span-5 last blog-sidebar">
-                
-                <?php
-                if (dynamic_sidebar('blog-sidebar')) :
-                else :
-                    ?>
-                <?php endif; ?>
-
-            </div>
+</div>
 
 
-            <?php get_footer(); ?>
+<!-- sidebar -->
+<div class="span-5 last blog-sidebar">
+
+    <?php
+    if (dynamic_sidebar('blog-sidebar')) :
+    else :
+        ?>
+    <?php endif; ?>
+
+</div>
+
+
+<?php get_footer(); ?>
