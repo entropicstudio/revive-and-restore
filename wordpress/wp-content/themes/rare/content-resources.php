d3d7b193b2a68e91d687075c6b45af60839686a1
Matthew <Matthew@Matthew-Laptop>
2013-10-24 11:12:13 -0600

    updated layout

--- wordpress/wp-content/themes/rare/content-resources.php
+++ wordpress/wp-content/themes/rare/content-resources.php
@@ -4,28 +4,15 @@
  */
 ?>
 
-<div class="span-20 prepend-1 append-1">
+<div class="span-20 prepend-1 append-1 padbottom-25">
     <?php the_content(); ?>
 </div>
 
 
 
 
-<div id="recommended-reading-box" class="span-13 prepend-1 append-1">
-    
-    <h2 class="section-heading">Recommended Reading</h2>
-    <hr />
+<div id="recommended-reading-box" class="span-20 prepend-1 append-1">
     
         <?php the_field('recommended_reading'); ?>
     
 </div> 
-
-
-<div id="relevant-links-box" class="span-6">
-    
-    <h2 class="section-heading">Relevant Links</h2>
-    <hr />
-    
-    <?php the_field('relevant_links'); ?>
-    
-</div>
